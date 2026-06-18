6898c97dedf9f75a750df48120ea4187a9b6f153
yeniferb <yeniferbuitrago9@gmail.com>
2026-06-18 18:21:01 -0500

    Update settings.py for deployment

--- GAMEDEX/settings.py
+++ GAMEDEX/settings.py
@@ -160,3 +160,22 @@ MEDIA_ROOT = BASE_DIR / 'media'
  
 
 ASGI_APPLICATION = "GAMEDEX.asgi.application"
+# -------------------------------------------------
+# LOGGING (para ver errores 500 en los logs de Render)
+# -------------------------------------------------
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
