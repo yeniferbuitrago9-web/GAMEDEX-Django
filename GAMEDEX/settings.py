9b4cdfdcbef10ae31ac7d5fa3b61188dc5f0cd7a
yeniferb <yeniferbuitrago9@gmail.com>
2026-06-22 09:37:47 -0500

    carga de imagenes

--- GAMEDEX/settings.py
+++ GAMEDEX/settings.py
@@ -183,8 +183,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # -------------------------------------------------
 
 MEDIA_URL = '/media/'
-
-MEDIA_ROOT = BASE_DIR / 'media'     
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
  
 
 ASGI_APPLICATION = "GAMEDEX.asgi.application"
